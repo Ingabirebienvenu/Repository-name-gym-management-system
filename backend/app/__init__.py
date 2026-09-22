7b0c871a12931237b446521c3fa967ab200f60ab
kenny523 <kennybienvenue2@gmail.com>
2026-09-22 00:32:34 -0700

    commit

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -13,12 +13,15 @@ def create_app():
     from app.routes.payment_routes import payment_bp
     from app.routes.attendance_routes import attendance_bp
     from app.routes.notification_routes import notification_bp
+    from app.routes.booking_routes import booking_bp
+
     app.register_blueprint(member_bp)
     app.register_blueprint(trainer_bp)
     app.register_blueprint(class_bp)
     app.register_blueprint(payment_bp)
     app.register_blueprint(attendance_bp)
     app.register_blueprint(notification_bp)
+    app.register_blueprint(booking_bp)
 
     @app.route('/')
     def index():
